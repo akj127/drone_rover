8246c2334971cf6f8723e40f30bd5852705c0821
siddharth <siddharth17196@iiitd.ac.in>
2020-12-21 18:47:10 +0530

    plotting the error

--- dronekit/plot_error.py
+++ dronekit/plot_error.py
@@ -1,15 +1,17 @@
+import matplotlib.pyplot as plt
+
 x = []
 y = []
 
 with open("drone.pos.out", 'r') as file:
 	for line in file:
 		line = line.split("\t")
-		x.append(float(line[0]))
-		y.append(float(line[1]))
-
+		try:
+			x.append(float(line[0]))
+			y.append(float(line[1]))
+		except ValueError:
+			print(line)
 zeros = [0] * len(x)
-
-import matplotlib.pyplot as plt
 
 plt.plot(x, label="x")
 plt.plot(y, label="y")
